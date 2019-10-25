3e866c9b1c20ba21dbd4ce65392c4009f2cb9b58
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-10-25 18:09:06 +0100

    Solution 2

--- src/002.sh
+++ src/002.sh
@@ -6,3 +6,26 @@
 #By considering the terms in the Fibonacci sequence whose
 # values do not exceed four million, 
 # find the sum of the even-valued terms.
+
+num1=1
+num2=1
+sum=0
+
+while [ $num2 -lt 4000000 ]
+
+do
+
+        if [ $(($num2 % 2)) -eq 0 ]
+
+        then
+
+                sum=$(($sum + $num2))
+
+        fi
+
+        num1=num2
+
+        num2=$(($num1 + $num2))
+done
+
+echo $sum
